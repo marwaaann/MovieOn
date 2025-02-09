654dc4613ff290abda5c071b94e1faf0f1cd9397
marwaaann <159703476+marwaaann@users.noreply.github.com>
2025-02-09 19:57:50 +0530

    adding render link to vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
     host: true,  // Allows external access (useful for Render)
-    port: process.env.PORT || 5173  // Use Render's assigned port or default to 5173
+    port: process.env.PORT || 5173,  // Use Render's assigned port or default to 5173
+    strictPort:true,
+    allowedHosts:["https://movieon-w4po.onrender.com"]
   }
 })
