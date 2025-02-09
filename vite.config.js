e68053af92149b2e513e2b0b1789fa3bf67fc38b
marwaaann <159703476+marwaaann@users.noreply.github.com>
2025-02-09 20:23:32 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Correct import for Tailwind
-import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     host: true,
     port: process.env.PORT || 5173,
@@ -14,7 +12,6 @@ export default defineConfig({
       credentials: true,
     }
   },
-  // Add preview configuration to handle the blocked host error
   preview: {
     allowedHosts: ['movieon-w4po.onrender.com'],
     port: process.env.PORT || 5173,
